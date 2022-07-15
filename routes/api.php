440f08c7772e724086fabff8553569302dce8106
iskandardevan <devvv13@gmail.com>
2022-07-15 14:15:36 +0700

    connect golang token

--- routes/api.php
+++ routes/api.php
@@ -13,23 +13,19 @@ use App\Http\Controllers\AuthController;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+*/ 
 
 Route::group([
     'prefix' => 'auth'
 ], function () {
-    Route::post('signin',AuthController::class . '@signin'); //Login    
-    Route::post('signup', AuthController::class . '@signup'); //Register
+    Route::post('signin', [AuthController::class , 'signin']); //Login    
+    Route::post('signup', AuthController::class . '@signup'); //Register 
 
     Route::group([
         'middleware' => 'auth:api'
     ], function() {
         Route::get('signout', AuthController::class . '@signout'); // Logout
-        Route::get('user', AuthController::class . '@user'); // get user info 
+        Route::get('user', [AuthController::class , 'user']); // Refresh token
         Route::put('user', AuthController::class . '@update'); // update user profile
         Route::delete('user', AuthController::class . '@destroy'); // delete user
     });
